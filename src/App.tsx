12bbef29b18449705602ba5c4819209ccbe2ecc0
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-11-10 16:16:20 +0100

    fetch dummy posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
+import { ReactNode, useEffect, useState } from "react";
 import { get } from "./util/http";
+import BlogPosts, { BlogPost } from "./components/BlogPosts.tsx";
+import fetchingImg from "./assets/data-fetching.png";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 function App() {
-  get();
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = await get<RawDataBlogPost[]>(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const blogPosts: BlogPost[] = data.map((blogPost) => {
+        return {
+          id: blogPost.id,
+          title: blogPost.title,
+          text: blogPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="fetching process" />
+      {content}
+    </main>
+  );
 }
 
 export default App;
